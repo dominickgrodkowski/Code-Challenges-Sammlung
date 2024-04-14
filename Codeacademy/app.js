19a731e12e0c96c41cc0ff3b4040839b93b2e8da
DomiGrod <Dominick.Grodkowski@gmx.de>
2024-04-14 15:20:27 +0200

    finished

--- Codeacademy/app.js
+++ Codeacademy/app.js
@@ -1,3 +1,4 @@
+// Introduction to JavaScript 
 console.log(5);
 console.log(8); 
 
@@ -14,5 +15,38 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 console.log('JavaScript');
 console.log(2011);
-console.log('Woohoo! I love to code! #codecademy');
+console.log('Woohoo! I love to code! #selfstudy');
 console.log(20.49);
+
+console.log(27+3.5)
+console.log(2024-1969)
+console.log(65/240)
+console.log(0.2708*100)
+
+console.log('Hello' + 'World')
+console.log('Hello' + ' ' + 'World')
+
+console.log('Teaching the world how to code'.length);
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+console.log(Math.random()*100)
+console.log(Math.floor(Math.random()*100))
+console.log(Math.ceil(43.8))
+console.log(Number.isInteger(2017))
+
+/*Let’s take one more glance at the concepts we just learned:
+
+Data is printed, or logged, to the console, a panel that displays messages, with console.log().
+We can write single-line comments with // and multi-line comments between /* and *//*.
+There are 7 fundamental data types in JavaScript: strings, numbers, booleans, null, undefined, symbol, and object.
+Numbers are any number without quotes: 23.8879
+Strings are characters wrapped in single or double quotes: 'Sample String'
+The built-in arithmetic operators include +, -, *, /, and %.
+Objects, including instances of data types, can have properties, stored information. The properties are denoted with a . after the name of the object, for example: 'Hello'.length.
+Objects, including instances of data types, can have methods which perform actions. Methods are called by appending the object or instance with a period, the method name, and parentheses. For example: 'hello'.toUpperCase().
+We can access properties and methods by using the ., dot operator.
+Built-in objects, including Math, are collections of methods and properties that JavaScript provides.*/
