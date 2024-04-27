8cd1eb1fc1dad4e219a6f72903db9d4692c1e64f
DomiGrod <Dominick.Grodkowski@gmx.de>
2024-04-28 00:53:24 +0200

    satrted

---
+++ Codeacademy/Arrays.js
@@ -0,0 +1,14 @@
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
+
+const hobbies = ['card games', 'reading', 'travel']
+console.log(hobbies);
+
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+const listItem = famousSayings[0]
+console.log(listItem);
+console.log(famousSayings[2]);
+console.log(famousSayings[3]);
+
