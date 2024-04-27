ce7e5eef6d7c8a11cffa06c92260c0b2e1aced61
DomiGrod <Dominick.Grodkowski@gmx.de>
2024-04-27 19:43:47 +0200

    created a const from function to global

--- TrainingDaysJS/app.js
+++ TrainingDaysJS/app.js
@@ -1,45 +1,43 @@
-// The scope of `random` is too loose 
+// The scope of `random` is too loose
+const name = "Nala";
 const random = Math.floor(Math.random() * 3);
 
 const getRandEvent = () => {
   if (random === 0) {
-    return 'Marathon';
+    return "Marathon";
   } else if (random === 1) {
-    return 'Triathlon';
+    return "Triathlon";
   } else if (random === 2) {
-    return 'Pentathlon';
+    return "Pentathlon";
   }
 };
 
-// The scope of `days` is too tight 
-const getTrainingDays = event => {
-let days 
-  if (event === 'Marathon') {
-     days = 50;
-  } else if (event === 'Triathlon') {
-     days = 100;
-  } else if (event === 'Pentathlon') {
-     days = 200;
+// The scope of `days` is too tight (fixed)
+const getTrainingDays = (event) => {
+  let days;
+  if (event === "Marathon") {
+    days = 50;
+  } else if (event === "Triathlon") {
+    days = 100;
+  } else if (event === "Pentathlon") {
+    days = 200;
   }
 
   return days;
 };
 
-// The scope of `name` is too tight 
-const logEvent = event => {
-  const name = 'Nala';
+// The scope of `name` is too tight (fixed)
+const logEvent = (name, event) => {
   console.log(`${name}'s event is: ${event}`);
 };
 
-const logTime = days => {
-  const name = 'Nala';
+const logTime = (name, days) => {
   console.log(`${name}'s time to train is: ${days} days`);
 };
 
 const event = getRandEvent();
 const days = getTrainingDays(event);
-// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
-
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime
 
 logEvent(event);
 logTime(days);
