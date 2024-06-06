236dd31e5e1a0a9e66daecbaffe3a33d813f5e87
DomiGrod <Dominick.Grodkowski@gmx.de>
2024-06-06 16:50:04 +0200

    1st commit

---
+++ first PHP test/functions.php
@@ -0,0 +1,26 @@
+<?php
+function praisePHP()
+{
+    echo "PHP is a great language!";
+
+}
+praisePHP();
+?>
+
+<?php
+function inflateEgo()
+{
+    echo "I am the best programmer!";
+}
+inflateEgo();
+?>
+
+<?php
+function printStringReturnNumber()
+{
+    echo "I am the best programmer!";
+    return 100;
+}
+$my_num = printStringReturnNumber();
+echo "\n$my_num";
+?>
