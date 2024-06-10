010d589c4037d2b9145d454a4b373a2a598d27c5
DomiGrod <Dominick.Grodkowski@gmx.de>
2024-06-10 15:00:41 +0200

    2nd update

--- first PHP test/functions.php
+++ first PHP test/functions.php
@@ -24,3 +24,79 @@ function printStringReturnNumber()
 $my_num = printStringReturnNumber();
 echo "\n$my_num";
 ?>
+
+<?php
+
+function notFound()
+{
+    echo "ERROR: Page not found!\n";
+    return 404;
+}
+
+function greetLearner()
+{
+    echo "Hello, Learner!\n";
+    echo "I hope you're (still) enjoying PHP!\n";
+    echo "Greetings, Dominick\n";
+    return "<3";
+}
+
+
+$error = notFound();
+$heart = greetLearner();
+
+echo "I received a $error, but it's ok because I also received $heart.";
+?>
+
+<?php
+function first()
+{
+    return "You did it!\n";
+}
+
+function second()
+{
+    return "You're amazing!\n";
+}
+
+function third()
+{
+    return "You're a coding hero!\n";
+}
+
+echo first() . second() . third();
+?>
+<?php
+function createVacuum()
+{echo "Creating a vacuum\n";}
+?>
+
+<?php
+function increaseEnthusiasm($name)
+{
+    return $name . "!";
+}
+echo increaseEnthusiasm("Dominick");
+?>
+
+<?php
+function repeatThreeTimes($hype)
+{
+    return $hype . $hype . $hype;
+}
+echo repeatThreeTimes("Hip ");
+
+echo increaseEnthusiasm(repeatThreeTimes("Hooray"));
+?>
+
+<?php
+function calculateArea($height, $width)
+{
+    return $height * $width;
+}
+echo calculateArea(5, 5);
+function calculateVolume($height, $width, $depth)
+{
+    return $height * $width * $depth;
+}
+echo calculateVolume(5, 5, 5);
